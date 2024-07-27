f6d571f890ee7b8ac9d5d3e2db898bf57508359b
soda7453 <sohail.danyal@tu-ilmenau.de>
2024-07-27 19:11:18 +0200

    New Files

---
+++ Basics/06_headerFile.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+// i have created a file, name this.h, which i have included as a header file in my program
+// Search for cpp reference
+#include "this.h"
+using namespace std;
+
+int main(){
+
+    cout<<"Hello world THis is my frist Programe"<<endl; // \n can be also be used for new line
+    return 0;
+
+}
