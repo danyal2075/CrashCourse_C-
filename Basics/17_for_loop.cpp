cc426488107bb316e5cf82750eb807921df2caa2
soda7453 <sohail.danyal@tu-ilmenau.de>
2024-08-04 03:39:34 +0200

    Loops in C++

---
+++ Basics/17_for_loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+// For loop
+int main(){
+    cout<<"For Loop"<<endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<i<<endl;
+    }
+    
+
+
+    return 0;
+}
---
+++ Basics/19_DoWhileLoop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+// do while loop: here loop will be run once even if condition is false
+int main(){
+    int i = 1;
+    do
+    {
+        cout<<i<<" * 6 = "<<i * 6<<endl;
+        i++;
+    } while (i<=10);
+    
+
+}
