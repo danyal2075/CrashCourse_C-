0481f0db9e4520d1edf09b311baeba01830fd5a6
soda7453 <sohail.danyal@tu-ilmenau.de>
2024-08-08 18:34:24 +0200

    inline functiona and static var

---
+++ Basics/33_static_variable_inFunction.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int product(int a, int b){
+    static int c;
+    c = c + 1;
+    return a * b + c;
+}
+
+int main(){
+    int a, b;
+    cout<<"Enter the value of a and b:  "<<endl;
+    cin>>a>>b;
+    cout<<"The prodcut of a and b is: "<<product(a, b)<<endl;
+    cout<<"The prodcut of a and b is: "<<product(a, b)<<endl;
+    cout<<"The prodcut of a and b is: "<<product(a, b)<<endl;
+    cout<<"The prodcut of a and b is: "<<product(a, b)<<endl;
+    cout<<"The prodcut of a and b is: "<<product(a, b)<<endl;
+    cout<<"The prodcut of a and b is: "<<product(a, b)<<endl;
+    cout<<"The prodcut of a and b is: "<<product(a, b)<<endl;
+    return 0;
+}
